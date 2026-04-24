c8bdd83bedeb8dac7c71dfda64cd06b35c66f1e3
dzakyputra45 <dzakypratama45@gmail.com>
2026-04-24 14:42:46 +0700

    Add web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ProductController;
 
 
 Route::get('/', function () {
@@ -17,11 +18,22 @@ Route::get('/about', [AboutController::class, 'index'])
     ->middleware(['auth'])
     ->name('about');
 
+Route::get('/product', function () {
+    return "Halaman Product";
+})->name('product')->middleware('auth');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+    Route::get('/product', [ProductController::class, 'index'])->name('product');
+    Route::get('/product/create', [ProductController::class, 'create'])->can('manage-product');
+    Route::post('/product', [ProductController::class, 'store'])->can('manage-product');
+    Route::get('/product/{product}/edit', [ProductController::class, 'edit']);
+    Route::put('/product/{product}', [ProductController::class, 'update']);
+    Route::delete('/product/{product}', [ProductController::class, 'destroy']);
 });
 
 require __DIR__.'/auth.php';
