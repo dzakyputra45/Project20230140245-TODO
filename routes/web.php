cb3a1cf2efb0cd40aee260927e4813717b01fba5
dzakyputra45 <dzakypratama45@gmail.com>
2026-04-25 21:15:31 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
 
 
 Route::get('/', function () {
@@ -14,13 +15,20 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware(['auth', 'can:manage-category'])->group(function () {
+    Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
+    Route::get('/category/create', [CategoryController::class, 'create'])->name('category.create');
+    Route::post('/category', [CategoryController::class, 'store'])->name('category.store');
+    Route::get('/category/{kategori}/edit', [CategoryController::class, 'edit'])->name('category.edit');
+    Route::put('/category/{kategori}', [CategoryController::class, 'update'])->name('category.update');
+    Route::delete('/category/{kategori}', [CategoryController::class, 'destroy'])->name('category.destroy');
+});
+
 Route::get('/about', [AboutController::class, 'index'])
     ->middleware(['auth'])
     ->name('about');
 
-Route::get('/product', function () {
-    return "Halaman Product";
-})->name('product')->middleware('auth');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -31,9 +39,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/product', [ProductController::class, 'index'])->name('product');
     Route::get('/product/create', [ProductController::class, 'create'])->can('manage-product');
     Route::post('/product', [ProductController::class, 'store'])->can('manage-product');
-    Route::get('/product/{product}/edit', [ProductController::class, 'edit']);
-    Route::put('/product/{product}', [ProductController::class, 'update']);
-    Route::delete('/product/{product}', [ProductController::class, 'destroy']);
+    Route::get('/product/{product}', [ProductController::class, 'show'])->name('product.show');
+    Route::get('/product/{product}/edit', [ProductController::class, 'edit'])->name('product.edit');
+    Route::put('/product/{product}', [ProductController::class, 'update'])->name('product.update');
+    Route::delete('/product/{product}', [ProductController::class, 'destroy'])->name('product.destroy');
 });
 
 require __DIR__.'/auth.php';
